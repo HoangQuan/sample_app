c54e6d8651cfd4004431c609bc4aac44206e9533
QUAN <quan@quan-K53SV.(none)>
2012-12-12 10:06:14 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '74e212861d95e6e0fb081fb7abb113b4a2b30b9d2a1c6672adda7bb83f941a7f858e7f3f05a66fb4e917e83f1904ea7e63919a5d453a9571f0324088afca8d95'
