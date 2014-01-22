5fe364589e9f6b718b2243dbc73c229f7ff4eb48
Hoang Quan <hoang.van.quan@framgia.com>
2014-01-22 09:07:56 +0700

    add download csv for product

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+
+  def headers_for_downloading_csv(filename = params[:controller])
+    response.headers["Content-Type"] = "text/csv"
+    response.headers["Content-Disposition"] = "attachment;" +
+      "filename=#{filename}.csv"
+  end
+  
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,13 @@
 class ProductsController < ApplicationController
 
   def index
-    @q = Product.order_by_updated.search params[:q]
-    @products = @q.result.page(params[:page])
+    respond_to do |format|
+      format.html do
+        @q = Product.order_by_updated.search params[:q]
+        @products = @q.result.page(params[:page])
+      end
+      format.csv {headers_for_downloading_csv}
+    end
   end
 
   def show
