fb935f2b8a6de654bdc0b6d919ef97c8180341e9
Hoang Quan <hoang.van.quan@framgia.com>
2014-02-07 14:53:46 +0700

    remove require File form config/deploy/production.rb to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,9 @@
-require "#{File.dirname(__FILE__)}/"
-
 set :rails_env, "production"
 set :deploy_env, "production"
 # Directories
 set :deploy_to, "/usr/local/rails_apps/sample_app"
 set :pid_file, "/tmp/unicorn_sample_app.pid"
 
-set :servers, get_ec2_targets("sample_app-production", !!ENV["OUT_OF_SERVICE"]).keys.to_a
-role :web do servers end
-role :app do servers end
-role :db, primary: true do servers.first end
+role :web, "localhost"
+role :app, "localhost"
+role :db, "localhost", primary: true
